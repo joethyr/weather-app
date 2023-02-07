5d60e4f01d0bb772c2d15436df8a8b97b8dad68d
joethyr <joethayr@gmail.com>
2023-02-07 07:19:04 -0700

    add webpack and eslint

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Is this darned thing working?")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLDRDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VhdGhlci1hcHAvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJJcyB0aGlzIGRhcm5lZCB0aGluZyB3b3JraW5nP1wiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
